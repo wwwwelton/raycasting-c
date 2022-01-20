1ad2043cf62deb851cc03b59e6e392a01c39f9d3
wleite <wleite@student.42sp.org.br>
2022-01-20 02:33:27 -0300

    refactor: replaced the while with SDL_Delay

--- src/main.c
+++ src/main.c
@@ -5,19 +5,20 @@
 SDL_Window		*window = NULL;
 SDL_Renderer	*renderer = NULL;
 int				isGameRunning = FALSE;
-int				ticksLastFrame = 0;
+int				ticksLastFrame;
 
 int				playerX;
 int				playerY;
 
-int	initializeWindow()
+int	initializeWindow(void)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		fprintf(stderr, "Error initializing SDL.\n");
 		return (FALSE);
 	}
-	window = SDL_CreateWindow(
+	window = SDL_CreateWindow
+	(
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
@@ -30,7 +31,6 @@ int	initializeWindow()
 		fprintf(stderr, "Error creating SDL window.\n");
 		return (FALSE);
 	}
-
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer)
 	{
@@ -42,73 +42,80 @@ int	initializeWindow()
 	return (TRUE);
 }
 
-void	destroyWindow()
+void	destroyWindow(void)
 {
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
 
-void	setup()
+void	setup(void)
 {
 	playerX = 0;
 	playerY = 0;
 }
 
-void	update()
-{
-	// waste some time until we reach the target frame time lenght
-	while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TIME_LENGTH));
-
-	float	deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
-
-	ticksLastFrame = SDL_GetTicks();
-
-	playerX += 50 * deltaTime;
-	playerY += 50 * deltaTime;
-}
-
-void	render()
-{
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
-	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-	SDL_Rect	rect = {playerX, playerY, 20, 20};
-	SDL_RenderFillRect(renderer, &rect);
-
-	SDL_RenderPresent(renderer);
-}
-
-void	processInput()
+void	processInput(void)
 {
 	SDL_Event	event;
-	SDL_PollEvent(&event);
 
+	SDL_PollEvent(&event);
 	switch (event.type)
 	{
 		case SDL_QUIT:
 		{
 			isGameRunning = FALSE;
-			break ;
+			break;
 		}
 		case SDL_KEYDOWN:
 		{
 			if (event.key.keysym.sym == SDLK_ESCAPE)
 				isGameRunning = FALSE;
-			break ;
+			break;
 		}
 	}
 }
 
-int	main()
+void	update(void)
+{
+	// Compute how long we have until the reach the target frame time in milliseconds
+	int timeToWait = FRAME_TIME_LENGTH - (SDL_GetTicks() - ticksLastFrame);
+
+	// Only delay execution if we are running too fast
+	if (timeToWait > 0 && timeToWait <= FRAME_TIME_LENGTH) {
+		SDL_Delay(timeToWait);
+	}
+
+	// Compute the delta time to be used as an update factor when changing game objects
+	float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+
+	// Store the milliseconds of the current frame to be used in the future
+	ticksLastFrame = SDL_GetTicks();
+
+	// Moves the rectangle
+	playerX += 50 * deltaTime;
+	playerY += 50 * deltaTime;
+}
+
+void	render(void)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+	SDL_Rect rect = {playerX, playerY, 20, 20};
+	SDL_RenderFillRect(renderer, &rect);
+
+	SDL_RenderPresent(renderer);
+}
+
+int	main(void)
 {
 	isGameRunning = initializeWindow();
 
 	setup();
 
-	while (isGameRunning)
-	{
+	while (isGameRunning) {
 		processInput();
 		update();
 		render();
